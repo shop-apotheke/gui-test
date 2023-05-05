b86e814dda566178cb17d3d766be22eadd929b83
aneja-arun1 <aneja.arun1@gmail.com>
2023-05-05 03:37:43 +0200

    Environment variable support for base url and browser.

--- GUITest/src/test/java/com/shopapotheke/test/TestProductAddition.java
+++ GUITest/src/test/java/com/shopapotheke/test/TestProductAddition.java
@@ -4,17 +4,28 @@ import java.util.concurrent.TimeUnit;
 import com.shopapotheke.pages.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.lang.System;
 public class TestProductAddition {
     WebDriver driver;
     HomePage homePage;
 
     @BeforeTest
     public void setup(){
-        String baseUrl = "https://www.shop-apotheke.com/";
-        driver = new ChromeDriver();
+        String baseUrl = System.getenv("SHOP_APOTHEKE_BASE_URL");
+        String browser = System.getenv("BROWSER");
+        switch (browser)
+        {
+            case "Chrome":
+                driver = new ChromeDriver();
+            case "Firefox":
+                driver = new FirefoxDriver();
+            default:
+                driver = new ChromeDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         homePage = new HomePage(driver, baseUrl);
